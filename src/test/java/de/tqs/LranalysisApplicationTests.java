7e380f85de53e15df8fa0e50252ced46bf988363
brixx-dl <langer.daniel@me.com>
2016-10-04 12:42:39 +0200

    Spring Boot App with Swagger

---
+++ src/test/java/de/tqs/LranalysisApplicationTests.java
@@ -0,0 +1,16 @@
+package de.tqs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LranalysisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
