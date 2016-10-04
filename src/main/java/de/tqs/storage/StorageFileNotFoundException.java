4d0c638772a7c36d0ae615bb2b7be235c7ec6150
brixx-dl <langer.daniel@me.com>
2016-10-04 13:36:35 +0200

    Implementiert FileUpload
    https://spring.io/guides/gs/uploading-files/

---
+++ src/main/java/de/tqs/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package de.tqs.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
