c497f2913921336327dd9ed26b0f6e790927c87f
brixx-dl <langer.daniel@me.com>
2016-10-05 15:35:00 +0200

    Added Cloud Foundry MySQL DataSource

---
+++ src/main/java/de/tqs/config/DataSourceConfig.java
@@ -0,0 +1,22 @@
+package de.tqs.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+	@Configuration
+	public class MySQLCloudConfig extends AbstractCloudConfig {
+
+	@Bean
+	public DataSource dataSource() {
+	  return connectionFactory().dataSource();
+	}
+
+	}
+	
+}
